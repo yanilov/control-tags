7124f855202a0ef822754a6325c3426230e011c9
Yoav Yanilov <yanilov@gmail.com>
2024-08-30 03:45:20 +0300

    tf: wip structured tag keys

--- terraform/control-tags/locals.tf
+++ terraform/control-tags/locals.tf
@@ -1,7 +1,9 @@
 locals {
   control_prefix          = "tagctl/"
-  grant_area_tag_key      = "${local.control_prefix}v1/meta/grant_path"
-  identity_broker_tag_key = "${local.control_prefix}v1/meta/id_broker"
-  mpa_tag_key             = "${local.control_prefix}v1/admin/mpa"
+  control_v1     = "${local.control_prefix}v1"
+
+  grant_area_tag_key      = "${local.control_v1}/meta/grant_area"
+  identity_broker_tag_key = "${local.control_v1}/meta/id_broker"
+  mpa_tag_key             = "${local.control_v1}/admin/mpa"
   approval_ticket_tag_key = "${local.mpa_tag_key}/ticket"
 }
