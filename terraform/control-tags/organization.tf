bce25736ccaba562a43aa2e84e0ae395fec67efb
Yoav Yanilov <yanilov@gmail.com>
2024-08-30 03:40:47 +0300

    tf: validation message

--- terraform/control-tags/organization.tf
+++ terraform/control-tags/organization.tf
@@ -4,7 +4,7 @@ data "aws_caller_identity" "main" {
   lifecycle {
     postcondition {
       condition     = self.account_id == data.aws_organizations_organization.main.master_account_id || var.using_delegated_admin_account
-      error_message = "this module must be called from the organization's management (AKA master) account."
+      error_message = "This module must be called from the organization's management (AKA master) account."
     }
   }
 }
