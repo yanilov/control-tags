9e7f4738e15ded3b6780669928300f59ba0b7784
Yoav Yanilov <yanilov@gmail.com>
2024-10-11 21:01:44 +0300

    rust: align tickets with terraform

--- rust/approval/src/tags.rs
+++ rust/approval/src/tags.rs
@@ -5,5 +5,4 @@ mod prefix {
 }
 
 pub(crate) const KEY_ADMIN: &str = concatcp!(prefix::CONTROL, "/", "admin");
-pub(crate) const KEY_ADMIN_TICKET: &str = concatcp!(KEY_ADMIN, "/", "ticket");
-pub(crate) const KEY_SYS_MPA: &str = concatcp!(prefix::CONTROL, "system", "/", "mpa");
+pub(crate) const KEY_ADMIN_TICKET: &str = concatcp!(KEY_ADMIN, "/", "mpa", "/", "ticket");
