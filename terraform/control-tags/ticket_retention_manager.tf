bc969a1ca0181781e0b4aa2c29583fcb0c644c71
Yoav Yanilov <yanilov@gmail.com>
2024-10-26 19:45:32 +0300

    tf: organize ticket retention files

--- terraform/control-tags/ticket_retention_manager.tf
+++ terraform/control-tags/ticket_retention_manager.tf
@@ -19,16 +19,14 @@ data "aws_iam_policy_document" "retention_manager" {
   }
   # allows the lambda (in manager mode) to invoke itself (in worker mode)
   statement {
-    sid = "InvokeSelf"
-
+    sid       = "InvokeLambdaAsWorker"
     effect    = "Allow"
     actions   = ["lambda:InvokeFunction"]
     resources = [local.retention_lambda_arn]
   }
   # allows the lambda (in worker mode) to assume the worker role
   statement {
-    sid = "AssumeWorkerRole"
-
+    sid       = "AssumeWorkerRole"
     effect    = "Allow"
     actions   = ["sts:AssumeRole"]
     resources = ["arn:aws:iam::*:role${local.retention_role.worker.path}${local.retention_role.worker.name}"]
@@ -54,9 +52,8 @@ resource "aws_iam_role" "retention_manager" {
 }
 
 resource "aws_iam_role_policy" "retention_manager" {
-  name = "ticket-retention-manager"
-  role = aws_iam_role.retention_manager.name
-
+  name   = "ticket-retention-manager"
+  role   = aws_iam_role.retention_manager.name
   policy = data.aws_iam_policy_document.retention_manager.json
 }
 
@@ -85,76 +82,5 @@ resource "aws_lambda_function" "retention" {
       "WORKER_ROLE_PATH"       = local.retention_role.worker.path
       "CONTROL_TAGS_SCP_ID"    = aws_organizations_policy.control_tags.id
     }
-  }
-}
-
-##### retention manager scheduling #####
-
-resource "aws_scheduler_schedule_group" "retention" {
-  name = local.retention_name_base
-}
-
-resource "aws_iam_role" "retention_scheduler" {
-  name = local.retention_role.scheduler.name
-  path = local.retention_role.scheduler.path
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "scheduler.amazonaws.com"
-        }
-        Condition = {
-          StringEquals = {
-            "aws:SourceAccount" = local.account_id
-            "aws:SourceArn"     = aws_scheduler_schedule_group.retention.arn
-          }
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "retention_scheduler" {
-  name = "ticket-retention-scheduler"
-  role = aws_iam_role.retention_scheduler.name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = ["lambda:InvokeFunction"]
-        Resource = aws_lambda_function.retention.arn
-      }
-    ]
-  })
-}
-
-
-resource "aws_scheduler_schedule" "retention" {
-  name        = local.retention_name_base
-  description = "A schedule to run the ticket retention lambda"
-  group_name  = aws_scheduler_schedule_group.retention.name
-
-  schedule_expression = "rate(${var.lambda_scheduler_rate_minutes} minutes)"
-  flexible_time_window {
-    mode = "OFF"
-  }
-
-  target {
-    arn      = "arn:aws:scheduler:::aws-sdk:lambda:invoke"
-    role_arn = aws_iam_role.retention_scheduler.arn
-
-    input = jsonencode({
-      FunctionName   = aws_lambda_function.retention.function_name
-      InvocationType = "Event"
-      Payload = jsonencode({
-        ScheduleApprovalEviction = {}
-      })
-    })
   }
 }
